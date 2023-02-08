fa4f00707d69a64122bbf27c5d6b9d9915ed8be8
Junior Alexandre da Silva <101154271+JuniorSilva88@users.noreply.github.com>
2023-02-07 23:13:07 -0300

    Dia - 4
    Finalizando o JavaScript, utilinado API
    api.openweathermap para busca de dados meteriológicos.
    
    Esse projeto teve a mentoira de Rodolfo Mori.

--- script.js
+++ script.js
@@ -3,24 +3,33 @@
 
 let chave = "cebcd482eda57fa9a6714c1c2ba91885"
 
+function exibirNaTela(dados){
+    console.log(dados)
+
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temperatura").innerHTML = Math.floor(dados.main.temp) + "°C , Mínima de " + Math.floor(dados.main.temp_min) + "°C e Máxima de " + Math.floor(dados.main.temp_max) + "°C"
+    document.querySelector(".descricao").innerHTML =  dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade de  " + dados.main.humidity + "%"
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+}
  
 async function encontrarCidade (cidade){ // funcão de acessar o servidor
-    let dados = await fetch()
-    ("https://api.openweathermap.org/data/2.5/weather?q=" + 
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
     cidade + 
     "&appid=" + 
     chave +
     "&lang=pt_br" +
-    "&unis=metric"
+    "&units=metric"
     )
     .then(respota => respota.json())
 
-    console.log(dados)
+    exibirNaTela(dados)
+
 }
 
 function cliqueiNoBotao(){
     let cidade = document.querySelector(".busca-cidade").value // funcão de buscar o nome da cidade digitada
 
-    buscaCidade(cidade)
-}
+    encontrarCidade(cidade)
 
+}
